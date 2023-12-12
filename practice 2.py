7748821fb836b03376db209a643650f646e56763
Sandendaty <39406572+Sandendaty@users.noreply.github.com>
2023-12-12 16:24:05 +0800

    practice 2.py

--- practice 2.py
+++ practice 2.py
@@ -11,16 +11,16 @@ html=requests.get(url).text
 from lxml import etree
 etree.HTMLParser(encoding='utf-8')
 doc=etree.HTML(html)
-reigon=doc.xpath('//*[@id="main"]/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[1]/span/text()')
+region=doc.xpath('//*[@id="main"]/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[1]/span/text()')
 infected=[int(i.replace(',','')) for i in doc.xpath('//*[@id="main"]/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[2]/text()')]
 dead=[int(i.replace(',','')) for i in doc.xpath('//*[@id="main"]/div[2]/div/div/div/div/div[1]/table/tbody/tr/td[3]/text()')]
 import pandas as pd
-df=pd.DataFrame({'reigons':reigon,'infected':infected,'dead':dead})
+df=pd.DataFrame({'regions':region,'infected':infected,'dead':dead})
 df2=df.sort_values(by='infected',ascending=False).iloc[0:15]
 import matplotlib
 matplotlib.rcParams['font.family']='simHei'
 import matplotlib.pyplot as plt
-x,y=df2['reigons'].map(lambda x:'\n'.join(x)),df2['infected']
+x,y=df2['regions'].map(lambda x:'\n'.join(x)),df2['infected']
 plt.bar(x,height=y,color=['red','orange','green','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4','#1f77b4'])
 plt.xlabel('美国的地区')
 plt.ylabel('确诊人数')
